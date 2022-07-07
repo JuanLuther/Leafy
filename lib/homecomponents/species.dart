3f5edcf448824c3bc2855b1d3ceffc2dcc37b683
JuanLuther <juanlutherhernando@gmail.com>
2022-07-07 15:16:46 +0800

    added species

--- lib/homecomponents/species.dart
+++ lib/homecomponents/species.dart
@@ -8,23 +8,32 @@ class Species extends StatelessWidget {
     return Hero(
       tag: "hero",
       child: Scaffold(
-        body: InkWell(
-          onTap: () => Navigator.of(context).push(
-              MaterialPageRoute(builder: ((context) => const MyWidget()))),
-          child: Card(
-            child: ListTile(
-              autofocus: true,
-              leading: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: Image.network(
-                  "https://images.dog.ceo/breeds/sharpei/noel.jpg",
-                  height: 100,
-                ),
-              ),
-              title: const Text("A dog"),
-              trailing: const Icon(Icons.place),
+        body: ListView(children: [
+          listElement(context, title: "title", subtitle: "subtitle"),
+        ]),
+      ),
+    );
+  }
+
+  InkWell listElement(BuildContext context,
+      {required String subtitle, required String title}) {
+    return InkWell(
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(
+          builder: ((context) =>
+              const MyWidget(title: "A dog", subtitle: "A dog subtitle")))),
+      child: Card(
+        child: ListTile(
+          autofocus: true,
+          leading: Padding(
+            padding: const EdgeInsets.all(2.0),
+            child: Image.network(
+              "https://images.dog.ceo/breeds/sharpei/noel.jpg",
+              height: 100,
             ),
           ),
+          title: Text(title),
+          subtitle: Text(subtitle),
+          trailing: const Icon(Icons.place),
         ),
       ),
     );
@@ -32,16 +41,24 @@ class Species extends StatelessWidget {
 }
 
 class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
-
+  const MyWidget({Key? key, required this.title, required this.subtitle})
+      : super(key: key);
+  final String title;
+  final String subtitle;
   @override
   Widget build(BuildContext context) {
     return Hero(
       tag: "hero",
       child: Scaffold(
-        appBar: AppBar(),
-        body: Image.network(
-          "https://images.dog.ceo/breeds/sharpei/noel.jpg",
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: ListView(
+          children: [
+            Image.network("https://images.dog.ceo/breeds/sharpei/noel.jpg"),
+            Center(child: Text(title)),
+            Center(child: Text(subtitle))
+          ],
         ),
       ),
     );
